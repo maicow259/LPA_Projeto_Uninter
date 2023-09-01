71e217927ac5057f65881ac1653455fdd04151fc
maicom araujo <maicow259@gmail.com>
2023-09-01 07:47:09 -0300

    Signed-off-by: maicom araujo <maicow259@gmail.com>

---
+++ DiagramaClasseJogo/Game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Game:
+    def __init__(self):
+        self.Attribute1 = None
+        self.window = None
+
+    def run(self, ):
+        pass
+
